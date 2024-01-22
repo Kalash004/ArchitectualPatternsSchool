37f865452c25473cfc518ad52b1fac10fdf67d38
Anton Kalashnikov <kalashnikov@SPSEJECNANET.CZ>
2024-01-22 14:41:30 +0100

    original

---
+++ u15.1b/three_tier_example/three_tier/application_tier.py
@@ -0,0 +1,38 @@
+class TaskListApplication():
+
+    def __init__(self):
+        self._is_running = False
+        self.database = None
+        self.user_interface = None
+
+    def run(self):
+        self.user_interface.print_message("Vítejte v programu na evidenci úkolů!")
+        self._is_running = True
+        while (self._is_running):
+            command = self.user_interface.menu_input()
+            command()
+
+    def terminate(self):
+        self._is_running = False
+        self.user_interface.print_message("Program ukončen, nashledanou příště.")
+
+    def show_task_list(self):
+        task_list = self.database.get_task_list()
+        self.user_interface.print_task_list(task_list)
+
+    def add_task(self):
+        new_task = self.user_interface.new_task_input()
+        self.database.add_task(new_task)
+
+    def remove_task_list(self):
+        if self.user_interface.remove_tasks():
+            self.database.remove_task_list()
+            self.user_interface.print_message("Seznam ukolů byl smazán.")
+
+    def show_first_task(self):
+        task = self.database.get_first_task()
+        self.user_interface.print_task_list([task])
+
+    def remove_first_task(self):
+        if self.user_interface.remove_first_task():
+            self.database.remove_first_task()
---
+++ u15.1c/mvc_example/mvc/model.py
@@ -0,0 +1,42 @@
+import os
+
+
+class TaskListModel():
+
+    def __init__(self, file_path, file_encoding):
+        self.file_path = file_path
+        self.file_encoding = file_encoding
+
+    def get_all(self):
+        tasks = list()
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r", encoding=self.file_encoding) as f:
+                tasks = f.readlines()
+        return tasks
+
+    def get_first(self):
+        task = ""
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r", encoding=self.file_encoding) as f:
+                task = f.readline()
+        return task
+
+    def add(self, new_task):
+        if os.path.exists(self.file_path):
+            mode = "a"
+        else:
+            mode = "w"
+        with open(self.file_path, mode, encoding=self.file_encoding) as f:
+            f.write(new_task + "\r\n")
+            f.flush()
+
+    def remove_all(self):
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
+
+    def remove_first(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r") as f:
+                lines = f.readlines()
+            with open("task_list.txt", "w") as f:
+                f.writelines(lines[1:])
